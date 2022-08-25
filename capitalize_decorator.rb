48d4f1d54ac13ccc91a0aed9a89e7cdb7c6759b2
aliatayee <aliaqaatayee@gmail.com>
2022-08-25 12:36:22 +0430

    Fixed required changes

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,7 +1,7 @@
-require './nameable'
-class CapitalizeDecorator < Nameable
+require './base_decorator'
+class CapitalizeDecorator < BaseDecorator
   def correct_name
-    super()
+    # Capitalize the first letter of the given name.
     @nameable.correct_name.capitalize
   end
 end
--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,7 +1,7 @@
-require './nameable'
-class TrimmerDecorator < Nameable
+require './base_decorator'
+class TrimmerDecorator < BaseDecorator
   def correct_name
-    super()
+    # The name has a maximum of 10 characters. If it's longer it should trim the word.`
     @nameable.correct_name.slice(0, 10)
   end
 end
