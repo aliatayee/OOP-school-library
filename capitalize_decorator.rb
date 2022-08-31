f413cade61680f09e4c8de06032be8231f2d819d
aliatayee <aliaqaatayee@gmail.com>
2022-08-31 18:43:28 +0430

    Update capitalize_decorator.rb

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,5 +1,9 @@
 require './base_decorator'
 class CapitalizeDecorator < BaseDecorator
+  def initialize(nameable = Nameable.new)
+    super()
+    @nameable = nameable
+  end
   def correct_name
     # Capitalize the first letter of the given name.
     @nameable.correct_name.capitalize
