41500d0e49aeb2d623002d953eca19f9376ea6e0
aliatayee <aliaqaatayee@gmail.com>
2022-09-01 17:26:00 +0430

    Update person.spec.rb test

--- spec/person.spec.rb
+++ spec/person.spec.rb
@@ -6,6 +6,7 @@ describe Person do
   context 'test Person class' do
     person = Person.new(24, 'Mechael Obama')
     person2 = Person.new(24)
+    person3 = Person.new(13,'Ahmad', parent_permission: false)
     book = Book.new('Becoming', 'Mechael Obama')
     rental = Rental.new('2022-05-13', person, book)
     it 'Book title should return Becoming' do
@@ -26,10 +27,13 @@ describe Person do
     it 'Person of_age should return true' do
       expect(person.send(:of_age?)).to be true
     end
-    it 'Person can_use_services should return true' do
+    it 'Person can_use_services should return true since default parameter is true' do
       expect(person.can_use_services?).to be true
     end
-    it 'Person name should return Mechael Obama' do
+    it 'Person can_use_services should return false since the age is less than 18' do
+      expect(person3.can_use_services?).to be false
+    end
+    it 'The constructor method set a default value for the optional name parameter if it is not provided' do
       expect(person2.name).to eq 'Unknown'
     end
   end
