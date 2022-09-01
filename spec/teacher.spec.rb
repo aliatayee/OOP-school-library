6676aa4ae088f91e7e0f6bf55e1940d824e22a83
jheart-vic <adebowalevictorjheart@gmail.com>
2022-09-01 10:29:37 +0100

    Add more test for methods

--- spec/teacher.spec.rb
+++ spec/teacher.spec.rb
@@ -3,6 +3,7 @@ require './teacher'
 describe Teacher do
   context 'test Teacher class' do
     teacher = Teacher.new(nil, 25, 'Ali ali')
+    teacher2 = Teacher.new('Maths', 25, 'Ali ali')
     it 'Teacher age should return 25' do
       expect(teacher.age).to eq 25
     end
@@ -11,6 +12,9 @@ describe Teacher do
     end
     it 'Person can_use_services should return true' do
       expect(teacher.can_use_services?).to be true
+    end
+    it 'Person specialization should return true' do
+      expect(teacher2.specialization).to eq 'Maths'
     end
   end
 end
