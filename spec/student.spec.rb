14975a7685651b62abfb73e11f15c1b1d37d8402
aliatayee <aliaqaatayee@gmail.com>
2022-09-01 14:04:34 +0430

    Add test for student parent permission

--- spec/student.spec.rb
+++ spec/student.spec.rb
@@ -2,12 +2,15 @@ require './student'
 
 describe Student do
   context 'test Student class' do
-    student = Student.new(nil, 25, 'Mechael Obama')
+    student = Student.new(nil, 25, 'Mechael Obama', parent_permission: false)
     it 'Student age should return 25' do
       expect(student.age).to eq 25
     end
     it 'Student name should return Mechael Obama' do
       expect(student.name).to eq 'Mechael Obama'
+    end
+    it 'Student parent_permission should return false' do
+      expect(student.parent_permission).to be false
     end
   end
 end
